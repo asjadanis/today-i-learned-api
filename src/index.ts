319f59bff36b33954dd6963d051950261b21bf94
asjadanis <asjadanis128@gmail.com>
2023-08-17 00:37:33 -0500

    feat(morgan): integrate morgan

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import config from "./configs/config";
+import morgan from "morgan";
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+} else {
+  app.use(morgan("combined"));
+}
 
 const PORT = config.PORT || 3001;
 
