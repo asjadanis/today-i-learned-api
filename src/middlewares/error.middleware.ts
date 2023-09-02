52f70e4d411431915ed8b3803a99861fce8d9795
asjadanis <asjadanis128@gmail.com>
2023-09-02 14:22:22 -0500

    fix(error-middleware): handle prisma errors

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,27 +1,63 @@
 import ApiError from "@/utils/ApiError";
+import httpStatus from "@/utils/httpStatus";
 import logger from "@/utils/logger";
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError
+} from "@prisma/client/runtime/library";
 import { NextFunction, Request, Response } from "express";
 
+type ErrorInstance = Error | ApiError | PrismaClientKnownRequestError;
+
 const errorHandler = (
-  err: Error | ApiError,
+  err: ErrorInstance,
   req: Request,
   res: Response,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
-  let error = err as ApiError;
+  let error: ApiError = err as ApiError;
 
-  if (error instanceof Error) {
-    const statusCode = error instanceof ApiError ? error.statusCode : 500;
-    const message = error.message || "Something went wrong!!!";
+  if (err instanceof PrismaClientKnownRequestError) {
+    switch (err.code) {
+      // Unique constraint violation
+      case "P2002":
+        error = new ApiError(
+          `Bad Request, unique constriant failed on ${err.meta?.target}`,
+          httpStatus.BAD_REQUEST
+        );
+        break;
+      // Record not found
+      case "P2025":
+        error = new ApiError(
+          "The requested record was not found.",
+          httpStatus.BAD_REQUEST
+        );
+        break;
+
+      default:
+        return res
+          .status(500)
+          .json({ message: "An unexpected database error occurred." });
+    }
+  } else if (err instanceof PrismaClientValidationError) {
+    const statusCode = httpStatus.BAD_REQUEST;
+    const message = "Bad Request, Validation failed.";
+    error = new ApiError(message, statusCode);
+  } else if (err instanceof Error) {
+    const statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    const message = "Something went wrong!!!";
     error = new ApiError(message, statusCode);
   }
 
   logger.error(error.message, error);
 
-  res
-    .status(error.statusCode || 500)
-    .json({ success: false, message: err.message });
+  res.status(error.statusCode).json({
+    success: false,
+    data: {
+      message: error.message
+    }
+  });
 };
 
 export default errorHandler;
