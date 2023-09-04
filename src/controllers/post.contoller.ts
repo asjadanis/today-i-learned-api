79d778e25a15b5780c0316b6900ddc196bc7351f
asjadanis <asjadanis128@gmail.com>
2023-09-04 17:41:06 -0500

    fix(post-api-response): fix api response

--- src/controllers/post.contoller.ts
+++ src/controllers/post.contoller.ts
@@ -5,30 +5,30 @@ import httpStatus from "@/utils/httpStatus";
 
 const getPosts = tryCatchWrapper(async (req: Request, res: Response) => {
   const posts = await postService.getPosts();
-  res.status(httpStatus.OK).json({ data: posts });
+  res.status(httpStatus.OK).json({ message: "success", data: posts });
 });
 
 const createPost = tryCatchWrapper(async (req: Request, res: Response) => {
   const post = await postService.createPost(req.body);
-  res.status(httpStatus.CREATED).json({ data: post });
+  res.status(httpStatus.CREATED).json({ message: "success", data: post });
 });
 
 const getPost = tryCatchWrapper(async (req: Request, res: Response) => {
   const { id } = req.params;
   const post = await postService.getPost(id);
-  res.status(httpStatus.OK).json({ data: post });
+  res.status(httpStatus.OK).json({ message: "success", data: post });
 });
 
 const updatePost = tryCatchWrapper(async (req: Request, res: Response) => {
   const { id } = req.params;
   const post = await postService.updatePost(id, req.body);
-  res.status(httpStatus.OK).json({ data: post });
+  res.status(httpStatus.OK).json({ message: "success", data: post });
 });
 
 const deletePost = tryCatchWrapper(async (req: Request, res: Response) => {
   const { id } = req.params;
   const post = await postService.deletePost(id);
-  res.status(httpStatus.OK).json({ data: post });
+  res.status(httpStatus.OK).json({ message: "success", data: post });
 });
 
 export { createPost, getPosts, getPost, updatePost, deletePost };
